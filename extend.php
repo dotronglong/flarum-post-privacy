fdb5062eac92160612706c47ae8a359ec803620a
Lucas <trong-long_do@astro.com.my>
2019-12-19 15:16:09 +0800

    Update locale, style

--- extend.php
+++ extend.php
@@ -3,6 +3,8 @@
 namespace LD\PostPrivacy;
 
 use Flarum\Extend;
+use Flarum\Discussion\Event\Saving;
+use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('admin'))
@@ -12,4 +14,7 @@ return [
         ->css(__DIR__.'/less/forum.less'),
     new Extend\Locales(__DIR__ . '/locale'),
     new \LD\PostPrivacy\Extend\Settings(),
+    function (Dispatcher $events) {
+        $events->listen(Saving::class, Listener\ProcessPostPrivacy::class);
+    }
 ];
